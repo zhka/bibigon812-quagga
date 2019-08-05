74d4e7bf17b697ac28f856bc74db8fdd2a9ba150
Konstantin A Zhuravlev aka ZhuKoV <zhukov@zhukov.int.ru>
2019-08-05 17:20:08 +0300

    Fix type-check of quagga::prefix_list for foreman

--- manifests/prefix_list.pp
+++ manifests/prefix_list.pp
@@ -1,7 +1,7 @@
 define quagga::prefix_list (
   Hash $rules = {},
 ) {
-  $rules.reduce({}) |Hash $rules, Tuple[Integer, Hash] $rule| {
+  $rules.reduce({}) |Hash $rules, Tuple[Variant[Integer, String], Hash] $rule| {
     merge($rules, { "${name} ${rule[0]}" => $rule[1] })
   }.each |String $prefix_list_name, Hash $prefix_list| {
     quagga_prefix_list {$prefix_list_name:
