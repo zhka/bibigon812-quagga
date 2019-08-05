f105c3ead7b172f2617cc86d3db3aaed2752bdfa
Konstantin A Zhuravlev aka ZhuKoV <zhukov@zhukov.int.ru>
2019-08-05 17:26:17 +0300

    Fix type-check of quagga::route_map for foreman

--- manifests/route_map.pp
+++ manifests/route_map.pp
@@ -1,7 +1,7 @@
 define quagga::route_map (
   Hash $rules = {},
 ) {
-  $rules.reduce({}) |Hash $rules, Tuple[Integer, Hash] $rule| {
+  $rules.reduce({}) |Hash $rules, Tuple[Variant[Integer, String], Hash] $rule| {
     merge($rules, { "${name} ${rule[0]}" => $rule[1] })
   }.each |String $route_map_name, Hash $route_map| {
     quagga_route_map {$route_map_name:
